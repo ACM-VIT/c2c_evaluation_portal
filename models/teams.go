9c63da87df27c78695cd2554634f5aa94965e878
Akshit Grover <akshit.grover2016@gmail.com>
2019-03-21 16:42:30 +0530

    Server -> Models: Update team model

--- models/teams.go
+++ models/teams.go
@@ -1,9 +1,16 @@
 package models
 
 type Teams struct {
-	names      []string
-	uniqueCode int
-	insp1      map[string]int
-	insp2      map[string]int
-	eval       []map[string]int
+	TeamId           string                    `bson:"teamId"`
+	Names            []string                  `bson:"names"`
+	Track            string                    `bson:"track"`
+	ProblemStatement string                    `bson:"problemStatement"`
+	UniqueCode       string                    `bson:"uniqueCode"`
+	Insp1            map[string]int            `bson:"insp1"`
+	Insp1Remarks     string                    `bson:"insp1Remarks"`
+	Insp2            map[string]int            `bson:"insp2"`
+	Insp2Remarks     string                    `bson:"insp2Remarks"`
+	Eval             map[string]map[string]int `bson:"eval"`
+	EvalRemarks      map[string]string         `bson:"evalRemarks"`
+	Eliminated       bool                      `bson:"eliminated"`
 }
