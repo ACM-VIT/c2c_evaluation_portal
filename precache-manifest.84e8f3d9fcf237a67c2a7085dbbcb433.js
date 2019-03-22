0e0503ee0fba0f128e87595a51582e6a43d5a2cd
ShivankSahai <shivank.sahai2017@vitstudent.ac.in>
2019-03-22 16:49:47 +0530

    Updates

---
+++ precache-manifest.84e8f3d9fcf237a67c2a7085dbbcb433.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcf032667e5b7aee9cf4",
+    "url": "/static/css/main.092e0a48.chunk.css"
+  },
+  {
+    "revision": "fcf032667e5b7aee9cf4",
+    "url": "/static/js/main.dd3e2871.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5bd30186dfdf37a146d7",
+    "url": "/static/js/2.2c028a4e.chunk.js"
+  },
+  {
+    "revision": "a8075c54944d6e2809dfb5114178f359",
+    "url": "/static/media/tr_green.a8075c54.svg"
+  },
+  {
+    "revision": "a32b9ad34af06a4c4bd603b9b290398e",
+    "url": "/static/media/bl_green.a32b9ad3.svg"
+  },
+  {
+    "revision": "edbb18f59a97cb04144d5da24f32ac2d",
+    "url": "/static/media/logo1.edbb18f5.png"
+  },
+  {
+    "revision": "6ed6bec4c1701631b6f865ade02050c0",
+    "url": "/static/media/back_img.6ed6bec4.svg"
+  },
+  {
+    "revision": "0462d4f9dc209efcdbd997da187ee997",
+    "url": "/static/media/logo.0462d4f9.svg"
+  },
+  {
+    "revision": "61f4494254ae4922184848532a1a322f",
+    "url": "/static/media/c2c_text.61f44942.svg"
+  },
+  {
+    "revision": "6b01cbb9535d2cdef1bab98533895f30",
+    "url": "/index.html"
+  }
+];
